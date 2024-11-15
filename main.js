91ca803f625873d82482749f20d8962a17d99fd1
Vishal Kumar Jangid <imrvishal28@gmail.com>
2024-11-15 13:05:11 +0530

    initialize slot machine project with prompt-sync and strip-ansi dependencies

---
+++ main.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount, try again.");
+    } else {
+      return numberDepositAmount;
+    }
+  }
+};
+
+const depositAmount = deposit();
