d2ffe2372e160e08fbceab42a0a04eac308ae33b
Vishal Kumar Jangid <imrvishal28@gmail.com>
2024-11-15 13:53:48 +0530

    refactor constants to uppercase and implement transpose and printRows functions for slot machine display

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const prompt = require("prompt-sync")();
 
-const rows = 3;
-const cols = 3;
+const ROWS = 3;
+const COLS = 3;
 
-const symbolsCount = {
+const SYMBOLS_COUNT = {
   A: 2,
   B: 4,
   C: 6,
   D: 8,
 };
 
-const symbolValues = {
+const SYMBOL_VALUES = {
   A: 5,
   B: 4,
   C: 3,
@@ -58,15 +58,16 @@ const getBet = (balance, lines) => {
 
 const spin = () => {
   const symbols = [];
-  for (const [symbol, count] of Object.entries(symbolsCount)) {
+  for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
     for (let i = 0; i < count; i++) {
       symbols.push(symbol);
     }
   }
-  const reels = [[], [], []];
-  for (let i = 0; i < cols; i++) {
+  const reels = [];
+  for (let i = 0; i < COLS; i++) {
+    reels.push([]);
     const reelSymbols = [...symbols];
-    for (let j = 0; j < rows; j++) {
+    for (let j = 0; j < ROWS; j++) {
       const randomIndex = Math.floor(Math.random() * reelSymbols.length);
       const selectedSymbol = reelSymbols[randomIndex];
       reels[i].push(selectedSymbol);
@@ -76,8 +77,33 @@ const spin = () => {
   return reels;
 };
 
-const reels = spin();
-console.log(reels)
+const transpose = (reels) => {
+  const rows = [];
+  for (let i = 0; i < ROWS; i++) {
+    rows.push([]);
+    for (let j = 0; j < COLS; j++) {
+      rows[i].push(reels[j][i]);
+    }
+  }
+  return rows;
+};
+
+const printRows = (rows) => {
+    for(const row of rows) {
+        let rowString = "";
+        for(const [i, symbol] of row.entries()) {
+            rowString += symbol
+            if(i != rows.length - 1) {
+                rowString += " | ";
+            }
+        }
+        console.log(rowString)
+    }
+}
+
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+const reels = spin();
+const rows = transpose(reels);
+printRows(rows);
